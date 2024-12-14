7c2298ef38ea35019f423172fb9b3008ce2a56ce
anushka-u <anushkau032@gmail.com>
2024-12-15 04:06:13 +0530

    Adding methods in roomRepository

--- RoomBookingService/src/main/java/com/roombooking/repository/RoomRepository.java
+++ RoomBookingService/src/main/java/com/roombooking/repository/RoomRepository.java
@@ -1,9 +1,18 @@
 package com.roombooking.repository;
 
 import com.roombooking.entity.Room;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room,Long> {
+
+	List<Room> findAllByStatus(String status);
+
+	List<Room> findAllByRoomType(String roomType);
+
+	Room findByRoomNumber(int roomNumber);
 }
