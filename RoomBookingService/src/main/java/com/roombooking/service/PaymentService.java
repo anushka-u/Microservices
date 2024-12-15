c0ab1cd35073259170a99f0c8ac9ef4030435a30
anushka-u <anushkau032@gmail.com>
2024-12-16 02:05:35 +0530

    Adding Payment Service

---
+++ RoomBookingService/src/main/java/com/roombooking/service/PaymentService.java
@@ -0,0 +1,38 @@
+package com.roombooking.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.roombooking.entity.Booking;
+import com.roombooking.entity.Payment;
+import com.roombooking.repository.BookingRepository;
+import com.roombooking.repository.PaymentRepository;
+import com.roombooking.service.BookingService.EntityNotFoundException;
+
+@Service
+public class PaymentService {
+
+	@Autowired
+	private PaymentRepository paymentRepository;
+	
+	@Autowired
+	private BookingRepository bookingRepository;
+	
+	public Payment doPayment(Payment payment)
+	{
+		if(payment.getBooking() == null)
+		{
+			throw new IllegalArgumentException("Booking doesn't exist");
+		}
+		Booking booking = bookingRepository.findById(payment.getBooking().getBookingId())
+				.orElseThrow(() -> new EntityNotFoundException("User not found"));
+		
+		Payment createPayment = new Payment();
+		createPayment.setAmountToPay(payment.getAmountToPay());
+		createPayment.setBooking(booking);
+		createPayment.setPaymentDate(payment.getPaymentDate());
+		createPayment.setPaymentStatus(payment.getPaymentStatus());
+		
+		return paymentRepository.save(createPayment);
+	}
+}
