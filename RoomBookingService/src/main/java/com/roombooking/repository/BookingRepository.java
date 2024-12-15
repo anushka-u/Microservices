2637d3aa99ab5ff5a81026d6f9a313a9fa5bae39
anushka-u <anushkau032@gmail.com>
2024-12-16 03:17:42 +0530

    adding query

--- RoomBookingService/src/main/java/com/roombooking/repository/BookingRepository.java
+++ RoomBookingService/src/main/java/com/roombooking/repository/BookingRepository.java
@@ -1,9 +1,14 @@
 package com.roombooking.repository;
 
 import com.roombooking.entity.Booking;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Long> {
+	
+	List<Booking> findByUser_UserId(Long userId);
 }
