0ce04d4cf2fd01a252f2b24a387adc06470e0afd
anushka-u <anushkau032@gmail.com>
2024-12-21 12:41:52 +0530

    Eureka server -> Servcie Registry.

---
+++ Service-Registry/src/test/java/com/eurekaserver/Service/Registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaserver.Service.Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
